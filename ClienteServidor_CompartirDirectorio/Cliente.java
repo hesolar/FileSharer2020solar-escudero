4d3f6131dacf00c207081b456852c2aed777d786
Héctor Solar Ruiz <43806203+hesolar@users.noreply.github.com>
2020-12-03 01:58:16 +0100

    c7 implementado el cd , ..,

--- ClienteServidor_CompartirDirectorio/Cliente.java
+++ ClienteServidor_CompartirDirectorio/Cliente.java
@@ -1,5 +1,51 @@
 package ClienteServidor_CompartirDirectorio;
 
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Cliente {
 
+	public static void main(String[] args) {
+		try {
+			
+			String  s1;
+
+			Scanner es = new Scanner(System.in);
+			boolean end=true;
+			while(end) {
+				System.out.println("\nINTRODUCE ORDEN\n");
+				s1=es.nextLine();
+				System.out.println(s1);
+				if(s1.equalsIgnoreCase("exit")){
+					
+					end=false;
+				}
+				else {
+					Socket s = new Socket("localhost",1111);
+					DataInputStream dis=new DataInputStream(s.getInputStream());
+					DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+					
+					dos.writeBytes(s1+"\r\n");
+					String a;
+					while((a=dis.readLine())!=null) {
+						System.out.println(a);
+						
+					}
+				}
+				
+			}
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 }
--- ClienteServidor_CompartirDirectorio/Servidor.java
+++ ClienteServidor_CompartirDirectorio/Servidor.java
@@ -1,12 +1,10 @@
 package ClienteServidor_CompartirDirectorio;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -14,20 +12,113 @@ import java.util.List;
 
 public class Servidor {
 
-	
-	public class Objetos implements Serializable{
-		private final long SerialVersionUID= 111L;
+	private static String path;
+	public Servidor(String s) {
+		this.path=s;
+	}
+	public String getPath() {
+		return this.path;
+	}
+	public class Objetos implements Serializable {
+		private final long SerialVersionUID = 111L;
 		private List<File> listado;
-		public Objetos(List<File> f) {this.listado=f;}
-		public List<File> getListado(){return this.listado;}
+
+		public Objetos(List<File> f) {
+			this.listado = f;
+		}
+
+		public List<File> getListado() {
+			return this.listado;
+		}
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
-		
+		ServerSocket ss;
+		try {
+			ss = new ServerSocket(1111);
+
+			while (true) {
+				try {
+					Socket s = ss.accept();
+					DataInputStream dis = new DataInputStream(s.getInputStream());
+					DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+					String linea = dis.readLine();
+					if (linea.contains("cd")) {
+						String a[] = linea.split(" ");
+
+						if(linea.contains("\\")) {
+						path = a[1];
+						
+						
+						}
+						
+						else {
+							path=path+"\\"+a[1];
+						}
+						
+						
+						dos.writeBytes("new Path:" +path+"\n");
+						File f = new File(path);
+						
+						if(f.listFiles()!=null) {
+						for (File x : f.listFiles()) {
+							dos.writeBytes(x.getName()+"\r\n");
+							
+						}dos.flush();
+						}
+						
+						
+						
+					}
+//
+					if (linea.equalsIgnoreCase("..")) {
+						File f = new File(path);
+						
+						f=f.getParentFile();
+								
+						path=f.getPath();
+						dos.writeBytes("new Path:" +path+"\n");
+						
+						List<String> names = new ArrayList<>();
+						for (File x : f.listFiles()) {
+							dos.writeBytes(x.getName()+"\r\n");
+							
+						}
+						dos.flush();
+						
+						
+						
+					} 
+					
+					if(linea.split(" ").length==1&&!linea.contains("..")) {
+
+						File f = new File(linea);
+						List<String> names = new ArrayList<>();
+						for (File x : f.listFiles()) {
+							dos.writeBytes(x.getName()+"\r\n");
+							
+						}
+						dos.flush();
+//						
+						path=linea;
+
+					}
+					if(linea.split(" ")==null) {
+						File f = new File(linea);
+					}
+					s.shutdownOutput();
+} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
-	
 
 }
