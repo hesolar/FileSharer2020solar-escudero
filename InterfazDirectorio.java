01a80165139ba98acda90e3e3996dc07f5ff2a12
mario <mario10tatu@hotmail.es>
2020-12-30 23:39:56 +0100

    c25 Interfaz Grafica Actualizada

--- InterfazDirectorio.java
+++ InterfazDirectorio.java
@@ -27,6 +27,7 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import java.awt.Font;
 import javax.swing.JSpinner;
+import javax.swing.JFormattedTextField;
 
 public class InterfazDirectorio {
 	
@@ -47,9 +48,19 @@ public class InterfazDirectorio {
 	private JTextField txfDirectorioActual;
 	private JTextField txfDirectorioDestino;
 	private JLabel lblDirectorioDondeSe;
-	private JLabel lblDirectorioDondeTe;
+	private JPanel pnlCliente;
 	private JPanel pnlServidor;
-
+	private JButton btnIr;
+	private JButton btnAtras;
+	private JComboBox cmbSeleccionarDirectorio;
+	private JComboBox comboBox_1;
+	private JLabel lblArchivos;
+	private JButton btnAñadir;
+	private JButton btnEliminar;
+	private JTextField textField;
+	private JLabel lblExpulsarClientes;
+	private JButton btnConectar;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -79,7 +90,8 @@ public class InterfazDirectorio {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 550, 350);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 692, 458);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
@@ -109,16 +121,18 @@ public class InterfazDirectorio {
 
 		txfPuerto = new JTextField();
 		txfPuerto.setColumns(10);
-		txfPuerto.setBounds(273, 10, 38, 20);
+		txfPuerto.setBounds(250, 10, 56, 20);
 		frame.getContentPane().add(txfPuerto);
 		
 		chbModoServidor = new Checkbox("Modo Servidor");
+		chbModoServidor.setFont(new Font("Dialog", Font.BOLD, 12));
 		
 		
 		chbModoServidor.addItemListener(c->chbStateChange(c));
 		
 		
 		chbmodoCliente = new Checkbox("Modo Cliente");
+		chbmodoCliente.setFont(new Font("Dialog", Font.BOLD, 12));
 		chbmodoCliente.addItemListener(e->	chbStateChange(e));
 			
 
@@ -127,40 +141,42 @@ public class InterfazDirectorio {
 		
 		
 		
-		chbModoServidor.setBounds(335, 10, 89, 22);
+		chbModoServidor.setBounds(437, 10, 112, 22);
 		frame.getContentPane().add(chbModoServidor);
-		chbmodoCliente.setBounds(441, 10, 80, 22);
+		chbmodoCliente.setBounds(555, 10, 105, 22);
 		frame.getContentPane().add(chbmodoCliente);
 		
 		JLabel lblIp = new JLabel("IP");
+		lblIp.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblIp.setBounds(21, 13, 15, 14);
 		frame.getContentPane().add(lblIp);
 		
 		JLabel lblPuerto = new JLabel("Puerto");
-		lblPuerto.setBounds(230, 13, 46, 14);
+		lblPuerto.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblPuerto.setBounds(206, 13, 46, 14);
 		frame.getContentPane().add(lblPuerto);
 		
-		JPanel pnlCliente = new JPanel();
+		pnlCliente = new JPanel();
 		pnlCliente.setBackground(Color.WHITE);
-		pnlCliente.setBounds(10, 41, 251, 259);
+		pnlCliente.setBounds(10, 41, 330, 377);
 		frame.getContentPane().add(pnlCliente);
 		pnlCliente.setLayout(null);
 		
 		JLabel lblCliente = new JLabel("Cliente");
 		lblCliente.setVerticalAlignment(SwingConstants.TOP);
 		lblCliente.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblCliente.setBounds(96, 11, 43, 14);
+		lblCliente.setBounds(135, 11, 43, 14);
 		pnlCliente.add(lblCliente);
 		lblCliente.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JLabel lblDirectorioActual = new JLabel("Directorio Actual");
 		lblDirectorioActual.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblDirectorioActual.setBounds(10, 99, 102, 14);
+		lblDirectorioActual.setBounds(10, 115, 102, 14);
 		pnlCliente.add(lblDirectorioActual);
 		
 		txfDirectorioActual = new JTextField();
 		txfDirectorioActual.setEditable(false);
-		txfDirectorioActual.setBounds(122, 96, 119, 20);
+		txfDirectorioActual.setBounds(123, 112, 197, 20);
 		pnlCliente.add(txfDirectorioActual);
 		txfDirectorioActual.setColumns(10);
 		
@@ -172,25 +188,61 @@ public class InterfazDirectorio {
 		txfDirectorioDestino = new JTextField();
 		txfDirectorioDestino.setEditable(false);
 		txfDirectorioDestino.setColumns(10);
-		txfDirectorioDestino.setBounds(122, 42, 119, 20);
+		txfDirectorioDestino.setBounds(122, 42, 198, 20);
 		pnlCliente.add(txfDirectorioDestino);
 		
 		lblDirectorioDondeSe = new JLabel("Directorio donde se guardar\u00E1n los archivos");
-		lblDirectorioDondeSe.setBounds(20, 70, 205, 14);
+		lblDirectorioDondeSe.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDirectorioDondeSe.setBounds(10, 70, 310, 31);
 		pnlCliente.add(lblDirectorioDondeSe);
 		
-		lblDirectorioDondeTe = new JLabel("Directorio del servidor en el que te encuentras");
-		lblDirectorioDondeTe.setBounds(20, 124, 231, 14);
-		pnlCliente.add(lblDirectorioDondeTe);
+		btnIr = new JButton("Ir");
+		btnIr.setBackground(Color.GREEN);
+		btnIr.setForeground(Color.BLACK);
+		btnIr.setFont(new Font("Tahoma", Font.BOLD, 8));
+		btnIr.setBounds(160, 155, 75, 23);
+		pnlCliente.add(btnIr);
+		
+		btnAtras = new JButton("Atras");
+		btnAtras.setBackground(Color.ORANGE);
+		btnAtras.setFont(new Font("Tahoma", Font.BOLD, 8));
+		btnAtras.setBounds(244, 155, 75, 23);
+		pnlCliente.add(btnAtras);
+		
+		cmbSeleccionarDirectorio = new JComboBox();
+		cmbSeleccionarDirectorio.setBounds(10, 154, 129, 22);
+		pnlCliente.add(cmbSeleccionarDirectorio);
+		
+		comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(10, 223, 129, 22);
+		pnlCliente.add(comboBox_1);
+		
+		lblArchivos = new JLabel("Selecionar Archivos");
+		lblArchivos.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblArchivos.setBounds(10, 198, 129, 14);
+		pnlCliente.add(lblArchivos);
+		
+		btnAñadir = new JButton("A\u00F1adir");
+		btnAñadir.setForeground(Color.BLACK);
+		btnAñadir.setFont(new Font("Tahoma", Font.BOLD, 8));
+		btnAñadir.setBackground(Color.GREEN);
+		btnAñadir.setBounds(160, 223, 75, 23);
+		pnlCliente.add(btnAñadir);
+		
+		btnEliminar = new JButton("Eliminar");
+		btnEliminar.setFont(new Font("Tahoma", Font.BOLD, 8));
+		btnEliminar.setBackground(Color.ORANGE);
+		btnEliminar.setBounds(244, 223, 75, 23);
+		pnlCliente.add(btnEliminar);
 		
 		pnlServidor = new JPanel();
 		pnlServidor.setBackground(Color.DARK_GRAY);
-		pnlServidor.setBounds(273, 41, 251, 259);
+		pnlServidor.setBounds(350, 41, 326, 377);
 		frame.getContentPane().add(pnlServidor);
 		pnlServidor.setLayout(null);
 		
 		JLabel lblServidor = new JLabel("Servidor");
-		lblServidor.setBounds(101, 11, 48, 14);
+		lblServidor.setBounds(137, 11, 48, 14);
 		lblServidor.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblServidor.setHorizontalAlignment(SwingConstants.CENTER);
 		lblServidor.setForeground(Color.WHITE);
@@ -203,17 +255,17 @@ public class InterfazDirectorio {
 		pnlServidor.add(lblClientesConectados);
 		
 		JComboBox cmbClientesConectados = new JComboBox();
-		cmbClientesConectados.setBounds(10, 65, 106, 22);
+		cmbClientesConectados.setBounds(10, 286, 106, 22);
 		pnlServidor.add(cmbClientesConectados);
 		
 		JLabel lblRecursosClonaados = new JLabel("Recursos Clonados");
 		lblRecursosClonaados.setForeground(Color.WHITE);
 		lblRecursosClonaados.setFont(new Font("Tahoma", Font.BOLD, 10));
-		lblRecursosClonaados.setBounds(135, 39, 106, 14);
+		lblRecursosClonaados.setBounds(172, 39, 106, 14);
 		pnlServidor.add(lblRecursosClonaados);
 		
 		txfRecursosClonados = new JTextField();
-		txfRecursosClonados.setBounds(126, 64, 115, 184);
+		txfRecursosClonados.setBounds(137, 65, 179, 288);
 		pnlServidor.add(txfRecursosClonados);
 		txfRecursosClonados.setColumns(10);
 		
@@ -221,50 +273,27 @@ public class InterfazDirectorio {
 		btnExpulsar.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnExpulsar.setBackground(Color.RED);
 		btnExpulsar.setForeground(new Color(0, 0, 0));
-		btnExpulsar.setBounds(20, 98, 89, 23);
+		btnExpulsar.setBounds(20, 319, 89, 23);
 		pnlServidor.add(btnExpulsar);
-
-	}
-	// HASTA AQUI BASICAMENTE
-
-	// tomar la ip y el puerto y crear el primer clienteservidor
-	//se comprueba que los numeros esten bien metidos 
-	private void clickBotonConectar(ActionEvent e) {
-
-		try {
-			ComprobarPuertoIp();
-			
-			
-			Integer port = Integer.parseInt(this.txfPuerto.getText());
-			this.Operador = new Cliente(port, ConstruirIp());
 		
-			if(this.chbmodoCliente.getState()) {
-				Operador.inicioCliente();
-				
-				actualizarDirectorios(Operador.getListaLectura());
-			}
-			else 							   Operador.inicioServidor();
-			
-			//Operador.inicioCliente();
-			//decidir si se desea iniciar el modo cliente o el modo servidor
-
-		}
-
-		catch (NumberFormatException f) {
-			JOptionPane.showMessageDialog(null,"Error en el formato númerico", "Error en formato númerico", 0, null);
-		}
-
-	}
-	
-	private void actualizarDirectorios(List<File> lf) {
+		textField = new JTextField();
+		textField.setBounds(10, 64, 106, 186);
+		pnlServidor.add(textField);
+		textField.setColumns(10);
 		
-		this.CmbDirectorio.removeAllItems();
-		for(File f:lf) {
-			
-			this.CmbDirectorio.addItem(f.getName());
-		}
+		lblExpulsarClientes = new JLabel("Expulsar Clientes");
+		lblExpulsarClientes.setForeground(Color.WHITE);
+		lblExpulsarClientes.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblExpulsarClientes.setBounds(10, 261, 106, 14);
+		pnlServidor.add(lblExpulsarClientes);
+		
+		btnConectar = new JButton("Conectar");
+		btnConectar.setBounds(326, 9, 92, 23);
+		frame.getContentPane().add(btnConectar);
+
 	}
-	
+
+
 	//comprueba que los formatos sean correctos
 	private boolean ComprobarPuertoIp() {
 		
@@ -331,16 +360,42 @@ public class InterfazDirectorio {
 		return b;
 		
 	}
+
+	// Comportaminto checkBox Modo cliente / servidor
 	
-	
-	//bloquear que el modo servidor y cliente se activen a la vez
 	private void chbStateChange(ItemEvent e) {
-		if(this.chbmodoCliente.getState()&&this.chbModoServidor.getState()) {
-			JOptionPane.showMessageDialog(null,"Error", "No puede activar ambos modos a la vez", 0, null);
-			Checkbox a= (Checkbox)e.getItemSelectable();
+		if (this.chbmodoCliente.getState() && this.chbModoServidor.getState()) {
+			JOptionPane.showMessageDialog(null, "Error", "No puede activar ambos modos a la vez", 0, null);
+			Checkbox a = (Checkbox) e.getItemSelectable();
 			a.setState(false);
 		}
 
-		
+		else {
+			if (!this.chbmodoCliente.getState() && !this.chbModoServidor.getState()) {
+				this.pnlCliente.show();
+				this.pnlServidor.show();
+
+			}
+
+			else {
+				if (this.chbmodoCliente.getState()) {
+					this.pnlCliente.enable();
+					this.pnlServidor.enable(false);
+
+					this.pnlCliente.show();
+					this.pnlServidor.hide();
+
+				}
+
+				if (this.chbModoServidor.getState()) {
+					this.pnlServidor.enable();
+					this.pnlCliente.enable(false);
+
+					this.pnlServidor.show();
+					this.pnlCliente.hide();
+				}
+			}
+		}
+
 	}
 }
